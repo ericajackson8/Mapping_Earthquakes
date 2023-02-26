cd5519fd428f8e23c56a7259ae100ec5600fd73f
ericajackson8 <56225030+ericajackson8@users.noreply.github.com>
2023-02-26 00:19:39 -0500

    new files

---
+++ Earthquake_Challenge/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiZXJpY2FqYWNrc29uOCIsImEiOiJjbGVldW5ka3owMGxxM3BwYzlkemZpYTVmIn0.y2YaniMvkMpYAuzne4mrhA";
---
+++ Mapping_GeoJSON_Points/static/js/logic_OnePoinGeoJson.js
@@ -0,0 +1,53 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level. Approx. Geographical Center ofUS
+let map = L.map('mapid').setView([30,30], 2);
+// We create the tile layer that will be the background of our map.
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        accessToken: API_KEY
+    });
+
+// Add GeoJSON Data
+
+let sanFranAirport =
+{
+    "type": "FeatureCollection", "features": [{
+        "type": "Feature",
+        "properties": {
+            "id": "3469",
+            "name": "San Francisco International Airport",
+            "city": "San Francisco",
+            "country": "United States",
+            "faa": "SFO",
+            "icao": "KSFO",
+            "alt": "14",
+            "tz-offset": "-8",
+            "dst": "A",
+            "tz": "America/Los_Angeles"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-122.375, 37.61899948120117]
+        }
+    }
+    ]
+};
+L.geoJSON(sanFranAirport,{
+    // We turn feature into a marker on the map
+    onEachFeature: function(feature, layer) {
+        console.log(layer);
+        layer.bindPopup("<h2>" + "Airport Code: " + feature.properties.faa + "</h2>");
+
+    }
+}).addTo(map)
+
+
+// Then we add our 'graymap' tile layer to the map.
+ streets.addTo(map);
+
+// createa a dark map
+// mapbox://styles/mapbox/dark-v11
